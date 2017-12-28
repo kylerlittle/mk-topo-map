4f06083d107f3465d68e0ee09f518b88fa616a13
kylerlittle <kyler.little@wsu.edu>
2017-12-27 16:11:26 -0800

    smoothing attempt; best_fig_3 looks damn close

--- progress.md
+++ progress.md
@@ -1,8 +1,9 @@
 # Updates
 * Fixed issue 1 by working on each image one at a time and resizing using `PIL.Image.NEAREST` filter. Check out this [link](https://github.com/kylerlittle/mk-topo-map/issues/1) for more details.
 * Fixed issue 2 by making it so maximum number of iterations may elapse before a pixel cluster is "off limits." Check out this [link](https://github.com/kylerlittle/mk-topo-map/issues/2) for more details.
-* Also, GREAT NEWS finally! Achieved my [first promising result](https://github.com/kylerlittle/mk-topo-map/tree/master/topo-maps). Compare *best_fig_1.png* to *best_fig_1_actual.jpg* and compare *best_fig_2.png* to *best_fig_2_actual.jpg* and you should find a lot of similarities. My program remarkable catchs some subtleties in the conformational changes of the fiber I didn't notice upon first glance. The figures are still quity noisy, but I think I'll be able to smooth them.
+* Also, GREAT NEWS finally! Achieved my [first promising result](https://github.com/kylerlittle/mk-topo-map/tree/master/topo-maps). Compare *best_fig_1.png* to *best_fig_1_actual.jpg* and compare *best_fig_2.png* to *best_fig_2_actual.jpg* and you should find a lot of similarities. My program remarkably catchs some subtleties in the conformational changes of the fiber I didn't notice upon first glance. The figures are still quity noisy, but I think I'll be able to smooth them.
 * Secondly, it appears that the models are converging to the object's shape in the variation of parameters (as it theoretically should). I still need to alter more parameters and attempt to smooth the figures, but I'm making excellent progress.
+* One really awesome thing I just noticed is that the xy dimensions in *best_fig_3.png* are legitimately accurate. When the model converges to the object in the z direction, it also converges in the xy direction. This is so cool.
 
 # Next Up
 1. Vary PIL's resizing filters since these clearly have a large impact on the outcome of this program.
@@ -20,4 +21,4 @@
 1. Make as user-friendly as possible.
 1. Update README.txt with explicit instructions.
 
-*Last Updated: 12/27/2017 11:26 PST*
+*Last Updated: 12/27/2017 16:09 PST*
--- threeD_plotting.py
+++ threeD_plotting.py
@@ -6,21 +6,21 @@ Created on Tue Mar  7 12:25:46 2017
 """
 
 #Libraries Included
-from Laplacian_Variance import variance_of_laplacian
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from scipy import interpolate
 from parameters import parameters
 
 def plot_threeDmodel(model, dimension_units, modelDir, p, testModeOn):
     print "[x] Graphing 3D Model"
 
-    
+    """
     # CODE FOR WIRE_FRAME PLOT:
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    ax = fig.add_subplot(111, projection='3d')  #111 means 1x1 block of graphs and subplot 1
     (Width,Height) = model.shape
     x = np.arange(Width) 
     x = x[10:31:1]
@@ -37,31 +37,36 @@ def plot_threeDmodel(model, dimension_units, modelDir, p, testModeOn):
     ax.set_zlabel('Height (' + dimension_units + ')', fontsize=10, color='blue')
     ax.view_init(elev=40, azim=-114)
     plt.show()
-
     """
     # Set up surface_plot.    
     fig = plt.figure()
     ax = fig.gca(projection='3d')
     
     # Set up grid and test data
-    (Width,Height) = model.shape
-    x = np.arange(Width)
-    x = x[10:31:1]
+    (Height,Width) = model.shape
     y = np.arange(Height)
-    Y, X = np.meshgrid(y, x)  # `plot_surface` expects `x` and `y` data to be 2D
-    print X.shape, Y.shape
+    y = y[10:31:1]
+    x = np.arange(Width)
+    X, Y = np.meshgrid(x, y)  # `plot_surface` expects `x` and `y` data to be 2D
     # 'meshgrid' converts coordinates to two separate 2D arrays
-    model = model[10:31:1][::]
-    print model.shape
-    Z = model.reshape(model.shape)  #flip plot upside down
-    surf = ax.plot_surface(X, Y, Z, cmap=cm.summer, linewidth=0, antialiased=False)
+    Z = model[10:31:1][::]
+    
+    xnew, ynew = np.mgrid[-1:1:560j, -1:1:84j]
+    tck = interpolate.bisplrep(X, Y, Z, s=0)
+    znew = interpolate.bisplev(xnew[:,0], ynew[0,:], tck)
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.summer, rstride=1, cstride=1,linewidth=0, antialiased=False)
     
     # Customize the z axis.
     ax.zaxis.set_major_locator(LinearLocator(10))    #evenly spaced ticks from min to max
-    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))    
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+    # Input matrix is rectangular, so set the aspect of the axis scaling to avoid a cubic plot
+    z_min = Z.min(); z_max = Z.max()
+    max_val = max(len(x), len(y))
+    ax.auto_scale_xyz([0, max_val], [0, max_val], [z_min - 5, z_max + 5])
     
     # Add a color bar which maps values to colors.
-    fig.colorbar(surf, shrink=0.5, aspect=5)    
+    fig.colorbar(surf, shrink=0.5, aspect=5)
     
     # Label my axes
     plt.title('Topographical Map', fontsize=14, color='blue')
@@ -76,7 +81,6 @@ def plot_threeDmodel(model, dimension_units, modelDir, p, testModeOn):
     #if testModeOn:   # Display figure rather than saving it.
     #plt.show()
     #else:          # Save fig with parameters in name & without surround whitespace
-    """
     """
     Naming standard: fig_middlePercentSavings(%)_cropThresholdLevel_resizingFilter_widthDivisor_heightDivisor_
                      startHeight_endHeight_maxAllowableIterations.png
